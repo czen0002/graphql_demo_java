9e9d7201a2767f210ae31c0f71817f8a184ebc1e
czen0002 <zc114789@gmail.com>
2024-06-22 23:27:09 +1000

    init

---
+++ src/main/java/io/czen/graphql_demo_java/GraphqlDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package io.czen.graphql_demo_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlDemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlDemoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/czen/graphql_demo_java/GraphqlDemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.czen.graphql_demo_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlDemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
